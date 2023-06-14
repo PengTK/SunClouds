9edcbc9e4efa7fcb1cf024ffab56d0477eb7323a
Future36 <mitya.lubianov@gmail.com>
2023-06-14 15:13:32 +0300

    Переработка оптимизации окна. Добавил скрипт для смены обоев (пока нигде не используется)

---
+++ ThemesLib/Selectors/ThemeSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ThemesLib.Selectors
+{
+    public class ThemeSelector : ResourceDictionary
+    {
+        public ThemeSelector()
+        {
+            DateTime currentTime = DateTime.Now;
+
+            string theme;
+            if (currentTime.Hour >= 4 && currentTime.Hour < 11)
+            {
+                theme = "Themes/DayTheme.xaml";
+            }
+            else if (currentTime.Hour >= 12 && currentTime.Hour < 23)
+            {
+                theme = "Themes/Morning-EveningTheme.xaml";
+            }
+            else
+            {
+                theme = "Themes/NightTheme.xaml";
+            }
+
+            ResourceDictionary resourceDictionary = new ResourceDictionary();
+            resourceDictionary.Source = new Uri(theme, UriKind.RelativeOrAbsolute);
+
+            MergedDictionaries.Add(resourceDictionary);
+        }
+    }
+}
