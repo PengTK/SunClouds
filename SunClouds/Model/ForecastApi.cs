eae5d4e5f4726bdbfabb4e5180f8e7d2ab206260
Steff <Steff@STEFFI>
2023-06-20 13:22:00 +0300

    добавлены картинки

--- SunClouds/Model/ForecastApi.cs
+++ SunClouds/Model/ForecastApi.cs
@@ -47,7 +47,8 @@ namespace SunClouds
 
         public class Weather
         {
-            public int id { get; set; }            
+            public int id { get; set; }          
+            public string main { get; set; }
             public string description { get; set; }
             public string icon { get; set; }
         }       
